99de63a64fb1a8c1e5a5ede1b0f2c1efa6a8a294
Dalibor Petrovich <daliborpskopje@gmail.com>
2021-12-22 00:51:27 +0100

    landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import gsap from "gsap";
+
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import "./styles/global.scss";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    const tl = gsap.timeline();
+
+    gsap.to("body", 0, { css: { visibility: "visible" } });
+
+    tl.from(".line span", 1.8, {
+      y: 100,
+      ease: "power4.out",
+      delay: 0.5,
+      skewY: 7,
+      stagger: {
+        amount: 0.3,
+      },
+    })
+      .to(".overlay", 1, {
+        opacity: 0,
+        delay: 0.4,
+      })
+      .to(".colorful", 0.5, { css: { color: "rgb(56, 255, 198)" } })
+      .to(".overlay", 0, { css: { display: "none" } });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="overlay"></div>
+      <Header />
+      <Banner />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row v-center right">
+          <div className="logo">
+            <a href="/">dalibor.</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
